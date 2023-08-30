c50b955aca8ac1f7f7473d4f2ac6a173212179b0
Dana Zack <danaezack@gmail.com>
2023-08-29 19:07:38 -0600

    Declare variables

---
+++ arrays.js
@@ -0,0 +1,5 @@
+// Declare variables
+var stringsArray = ["Nicole", "Holly", "Michie", "Tommy", "Christie"];
+var numbersArray = [88, 89, 90, 91, 92];
+var booleansArray = [true, true, true, false];
+
